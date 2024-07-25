0bb294b83fb7f3b0ded6be6d9c4042b4b1f71972
markat1 <markt@outlook.dk>
2024-07-25 12:01:29 +0000

    moved code into a main - now also evaluates the model

--- train.py
+++ train.py
@@ -1,14 +1,15 @@
 import warnings
-import numpy as np
 import pandas as pd
-import pickle
+import os
+import numpy as np
+import argparse
+import joblib
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import StandardScaler
 
 def standardize(self, X_train, X_test):
         """
@@ -24,21 +25,65 @@ def standardize(self, X_train, X_test):
         X_test = scaler.transform(X_test)
         return X_train, X_test, scaler
 
+def evaluate_model(self, y_test, y_pred):
+        mae = mean_absolute_error(y_test, y_pred)
+        mse = mean_squared_error(y_test, y_pred)
+        rmse = np.sqrt(mse)
+        r2 = r2_score(y_test, y_pred)
+
+        print("Mean Absolute Error (MAE):", round(mae, 2))
+        print("Mean Squared Error (MSE):", round(mse, 2))
+        print("Root Mean Squared Error (RMSE):", round(rmse, 2))
+        print("R-squared (R2) Score:", round(r2, 2))
+
 
 # Suppress warnings
 warnings.filterwarnings("ignore")
 
-# could have had a preprocessing step here but chose to take the cleaned data directly
-train_data = pd.read_csv("./data/data_cleaned.csv")
 
-X = train_data.drop('Time_taken(min)', axis=1)
-y = train_data['Time_taken(min)']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-X_train, X_test, scaler = standardize(X_train, X_test)
+def main(args):
+    # could have had a preprocessing step here but chose to take the cleaned data directly
+    train_data = pd.read_csv("./data/data_cleaned.csv")
 
-model = xgb.XGBRegressor(n_estimators=20, max_depth=9)
-model.fit(X_train, y_train)
+    environment = os.getenv("ENVIRONMENT", "local")
+    output_dir = "/opt/ml/model"
 
-y_pred = model.predict(X_test)
+    if environment == "local":
+        train_data_path = "data/train/train.csv"
+        output_dir = "output"
 
+    X = train_data.drop('Time_taken(min)', axis=1)
+    y = train_data['Time_taken(min)']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    X_train, X_test, scaler = standardize(X_train, X_test)
+
+    model = xgb.XGBRegressor(n_estimators=20, max_depth=9)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    evaluate_model(y_test, y_pred)
+
+    # Save model
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # Save model
+    model_path = os.path.join(output_dir, "model.joblib")
+    joblib.dump(model, model_path)
+
+    print(f"Model saved to {model_path}")
+
+    if __name__ == "__main__":
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "--train",
+            type=str,
+            help="Path to the training data",
+            default="/opt/ml/input/data/train",
+        )
+        parser.add_argument("--n-estimators", type=int, default=100)
+
+        args = parser.parse_args()
+        main(args)
